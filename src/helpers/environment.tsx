938822b58434f44dcb726160e69bba0a01cbe9c5
Ryan Krenzke <ryan.krenzke@gmail.com>
2020-12-04 15:38:58 -0500

    environment url fix

--- src/helpers/environment.tsx
+++ src/helpers/environment.tsx
@@ -5,7 +5,7 @@ switch (window.location.hostname){
     case 'localhost' || '127.0.0.1':
         APIURL = 'http://localhost:4000';
         break;
-    case 'micampfire.herokuapp.com' :
+    case 'micampfireapp.herokuapp.com' :
         APIURL = 'https://micampfire.herokuapp.com'
 }
 
